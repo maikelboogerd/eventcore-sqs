86194aef4ca2bf2a5fe25e61bf2bc6af134541de
Maikel <maikelboogerd@gmail.com>
2018-07-16 21:04:55 +0200

    Bump for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='eventcore-sqs',
-    version='0.0.1',
+    version='0.3.0',
     description='Produce and consume events with SQS.',
     author='Maikel van den Boogerd',
     author_email='maikelboogerd@gmail.com',
